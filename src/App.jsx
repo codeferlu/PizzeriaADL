783fadfb744aaba93826f56fea2cda37b790e37c
Fernando <codeferlu@gmail.com>
2024-03-06 15:50:00 -0300

    fetchpizzas()

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,23 @@ import Error from './views/Error'
 import {Route, Routes} from 'react-router'
 
 const App = () => {
-  const [selectedPizza, setSelectedPizza] = useState("napolitana")
+  const [selectedPizzas, setSelectedPizzas] = useState("napolitana")
   const [dataPizzas, setDataPizzas] = useState([])
 
   const fetchPizzas = async () =>  {
      const data = await fetch("/pizzas.json")
-     const res = await data.json();
-     setDataPizzas(res);
+     const res = await data.json()
+     setDataPizzas(res)
   }
+
   useEffect (() => {
-fetchPizzas
+fetchPizzas()
   }, [])
   return (
 <>
 <Routes>
   <Route path="/" element={<Home/>}/>
-  <Route path="/pizzas/:selectedPizza" element={<Pizzas data={dataPizzas}/>}/>
+  <Route path="/pizzas/:selectedPizzas" element={<Pizzas data={dataPizzas}/>}/>
   <Route path="/carrito" element={<Carrito/>}/>
   <Route path="*" element={<Error/>}/>
 </Routes>
