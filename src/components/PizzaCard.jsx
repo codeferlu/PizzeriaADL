4bd28d5cbfcedbe42815e620d1acb3c50213bc41
Fernando <codeferlu@gmail.com>
2024-03-07 08:24:03 -0300

    Cards

--- src/components/PizzaCard.jsx
+++ src/components/PizzaCard.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { ContextApp } from "../App";
-
+import Card from 'react-bootstrap/Card';
 
 const PizzaCard = (props) => {
     const c = useContext(ContextApp)
@@ -21,23 +21,28 @@ c.setDataPizzas(updatedPizzas)
 }
 
     return (
-  
-<li>
-  <h2>{props.name}</h2>
-  <img src={props.img} alt={props.name}/>
-  <h2>{props.desc}</h2>
-  <h2>${props.price}</h2>
+  <div className="cajaCarta">
+<li className="li">
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={props.img} />
+  <Card.Body>
+<Card.Title>{props.name}</Card.Title>
+  <Card.Text>{props.desc}</Card.Text>
+  <Card.Text>${props.price}</Card.Text>
   <ul>
     {props.ingredients.map((ingredient, index) => (
       <li key={index}>{ingredient}</li>
     ))}
   </ul>
-  <span>Llevas en el carrito: {props.count}</span>
-  <button disabled={props.count == 0 ? true : false} onClick={() => handlePizza(props.name, 'remove')}>Quitar del carrito</button>
   <button onClick={() => handlePizza(props.name, 'add')}>Agregar al Carrito</button> 
+  <button disabled={props.count == 0 ? true : false} onClick={() => handlePizza(props.name, 'remove')}>Quitar del carrito</button>
+  </Card.Body>
+  </Card>
+  <span>Llevas en el carrito: {props.count}</span>
 </li>
-
+</div>
     );
   };
   export default PizzaCard;
   
+
