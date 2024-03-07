7019ca62c651b240e4935e312264488e7fc0e07b
Fernando <codeferlu@gmail.com>
2024-03-07 09:15:12 -0300

    Cambios UI

--- src/components/PizzaCard.jsx
+++ src/components/PizzaCard.jsx
@@ -21,26 +21,28 @@ c.setDataPizzas(updatedPizzas)
 }
 
     return (
-  <div className="cajaCarta">
-<li className="li">
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={props.img} />
-  <Card.Body>
-<Card.Title>{props.name}</Card.Title>
-  <Card.Text>{props.desc}</Card.Text>
-  <Card.Text>${props.price}</Card.Text>
-  <ul>
-    {props.ingredients.map((ingredient, index) => (
-      <li key={index}>{ingredient}</li>
-    ))}
-  </ul>
-  <button onClick={() => handlePizza(props.name, 'add')}>Agregar al Carrito</button> 
-  <button disabled={props.count == 0 ? true : false} onClick={() => handlePizza(props.name, 'remove')}>Quitar del carrito</button>
-  </Card.Body>
-  </Card>
-  <span>Llevas en el carrito: {props.count}</span>
-</li>
-</div>
+  <div className="caja">
+        <li className="li">
+        <ul className="ul">
+            <Card border="light" className="bg-dark text-white box" style={{ width: '10rem', margin: '0.5rem' } }>
+                <Card.Img variant="top" src={props.img} />
+                <Card.Body>
+                    <Card.Title>{props.name}</Card.Title>
+                    <ul>
+                        {props.ingredients.map((ingredient, index) => (
+                            <li key={index}>{ingredient}</li>
+                            ))}
+                    </ul>
+                            <Card.Text>${props.price}</Card.Text>
+                    <button onClick={() => handlePizza(props.name, 'add')}>Agregar al Carrito</button> 
+                    <button disabled={props.count == 0 ? true : false} onClick={() => handlePizza(props.name, 'remove')}>Quitar del carrito</button>
+                    <Card.Text>Llevas en el carrito: {props.count}</Card.Text>
+                </Card.Body>
+            </Card>
+        </ul>
+    </li>
+    </div>
+
     );
   };
   export default PizzaCard;
